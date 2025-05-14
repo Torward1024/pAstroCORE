46cd9e09c85d4343d73b1888f170c3870c4258e6
Torward1024 <almax1024@gmail.com>
2025-05-14 15:10:43 +0300

    14.05.2025

--- pastrocore/gui/ui_dialog_about.py
+++ pastrocore/gui/ui_dialog_about.py
@@ -84,7 +84,7 @@ class Ui_AboutDialog(object):
         AboutDialog.setWindowTitle(QCoreApplication.translate("AboutDialog", u"About pAstroCORE", None))
         AboutDialog.setStyleSheet(QCoreApplication.translate("AboutDialog", u"background-color: #ffffff; font-family: Arial;", None))
         self.labelTitle.setText(QCoreApplication.translate("AboutDialog", u"pAstroCORE", None))
-        self.labelVersion.setText(QCoreApplication.translate("AboutDialog", u"Version 0.0.8a", None))
+        self.labelVersion.setText(QCoreApplication.translate("AboutDialog", u"Version 0.0.8b", None))
         self.labelDescription.setText(QCoreApplication.translate("AboutDialog", u"A versatile tool for VLBI observation planning and visualization.", None))
         self.label.setText(QCoreApplication.translate("AboutDialog", u"Ballistics Laboratory, Astro Space Center LPI, 2018-2025", None))
         self.closeButton.setText(QCoreApplication.translate("AboutDialog", u"Close", None))
