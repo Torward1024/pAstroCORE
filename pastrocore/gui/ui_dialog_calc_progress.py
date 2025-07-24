c5a8487799b5f974b64ee6088503e75a372c1e1f
Torward1024 <almax1024@gmail.com>
2025-07-24 16:39:51 +0300

    24.07.2025

--- pastrocore/gui/ui_dialog_calc_progress.py
+++ pastrocore/gui/ui_dialog_calc_progress.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dialog_calc_progressBnNEsU.ui'
+## Form generated from reading UI file 'dialog_calc_progressFdnAas.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.2
 ##
@@ -15,35 +15,82 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QFrame, QGridLayout,
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QHBoxLayout,
     QLabel, QProgressBar, QPushButton, QSizePolicy,
     QSpacerItem, QWidget)
 
-class Ui_CalcDialog(object):
-    def setupUi(self, CalcDialog):
-        if not CalcDialog.objectName():
-            CalcDialog.setObjectName(u"CalcDialog")
-        CalcDialog.setWindowModality(Qt.WindowModality.ApplicationModal)
-        CalcDialog.resize(450, 130)
+class Ui_ProgressDialog(object):
+    def setupUi(self, ProgressDialog):
+        if not ProgressDialog.objectName():
+            ProgressDialog.setObjectName(u"ProgressDialog")
+        ProgressDialog.setWindowModality(Qt.WindowModality.WindowModal)
+        ProgressDialog.resize(450, 109)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(CalcDialog.sizePolicy().hasHeightForWidth())
-        CalcDialog.setSizePolicy(sizePolicy)
-        CalcDialog.setMinimumSize(QSize(450, 130))
-        CalcDialog.setMaximumSize(QSize(450, 130))
+        sizePolicy.setHeightForWidth(ProgressDialog.sizePolicy().hasHeightForWidth())
+        ProgressDialog.setSizePolicy(sizePolicy)
+        ProgressDialog.setMinimumSize(QSize(450, 100))
+        ProgressDialog.setMaximumSize(QSize(450, 109))
         icon = QIcon()
-        icon.addFile(u":/icons/calculate.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        CalcDialog.setWindowIcon(icon)
-        CalcDialog.setStyleSheet(u"background-color: #ffffff; font-family: Arial;")
-        CalcDialog.setModal(True)
-        self.gridLayout = QGridLayout(CalcDialog)
+        icon.addFile(u":/icons/preferences.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        ProgressDialog.setWindowIcon(icon)
+        ProgressDialog.setStyleSheet(u"background-color: #ffffff; font-family: Arial;")
+        ProgressDialog.setModal(True)
+        self.gridLayout = QGridLayout(ProgressDialog)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.pushButton = QPushButton(CalcDialog)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setStyleSheet(u"QPushButton {\n"
+        self.progressBar = QProgressBar(ProgressDialog)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setMinimumSize(QSize(0, 35))
+        self.progressBar.setMaximumSize(QSize(450, 35))
+        self.progressBar.setStyleSheet(u"/* \u0421\u0442\u0438\u043b\u0438\u0437\u0430\u0446\u0438\u044f QProgressBar */\n"
+"QProgressBar {\n"
+"    font-family: Arial;\n"
+"    font-size: 9pt;\n"
+"    color: #333333;\n"
+"    background-color: #f9f9f9; /* \u0424\u043e\u043d \u043d\u0435\u0430\u043a\u0442\u0438\u0432\u043d\u044b\u0445 \u044d\u043b\u0435\u043c\u0435\u043d\u0442\u043e\u0432 */\n"
+"    border: 1px solid #d3d3d3; /* \u0413\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435\u0430\u043a\u0442\u0438\u0432\u043d\u043e\u0433\u043e \u0441\u043e\u0441\u0442\u043e\u044f\u043d\u0438\u044f */\n"
+"    border-radius: 3px; /* \u0421\u043a\u0440\u0443\u0433\u043b\u0435\u043d\u043d\u044b\u0435 \u0443\u0433\u043b\u044b */\n"
+"    text-align: center; /* \u0412\u044b\u0440\u0430\u0432\u043d\u0438\u0432\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430 \u043f\u043e \u0446\u0435\u043d\u0442\u0440\u0443 */\n"
+"}\n"
+"/* \u0421\u0442\u0438\u043b\u0438\u0437\u0430\u0446\u0438\u044f \u0437\u0430\u043f\u043e\u043b\u043d\u0435\u043d\u043d\u043e\u0439 \u0447\u0430"
+                        "\u0441\u0442\u0438 \u043f\u0440\u043e\u0433\u0440\u0435\u0441\u0441-\u0431\u0430\u0440\u0430 */\n"
+"QProgressBar::chunk {\n"
+"    background-color: #0078d7; /* \u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u0430\u043a\u0446\u0435\u043d\u0442\u043d\u044b\u0439 \u0446\u0432\u0435\u0442 */\n"
+"    border-radius: 2px; /* \u0421\u043a\u0440\u0443\u0433\u043b\u0435\u043d\u0438\u0435 \u0432\u043d\u0443\u0442\u0440\u0438, \u0447\u0442\u043e\u0431\u044b \u0443\u0447\u0438\u0442\u044b\u0432\u0430\u0442\u044c \u0433\u0440\u0430\u043d\u0438\u0446\u0443 */\n"
+"}\n"
+"/* \u0410\u043d\u0438\u043c\u0430\u0446\u0438\u044f \u0434\u043b\u044f \u043f\u043b\u0430\u0432\u043d\u043e\u0433\u043e \u0438\u0437\u043c\u0435\u043d\u0435\u043d\u0438\u044f \u043f\u0440\u043e\u0433\u0440\u0435\u0441\u0441\u0430 */\n"
+"QProgressBar::chunk {\n"
+"    transition: width 0.2s ease-in-out; /* \u041f\u043b\u0430\u0432\u043d\u043e\u0435 \u0438\u0437\u043c\u0435\u043d\u0435\u043d\u0438\u0435 \u0448\u0438\u0440\u0438\u043d\u044b \u0437\u0430\u043f\u043e"
+                        "\u043b\u043d\u0435\u043d\u0438\u044f */\n"
+"}")
+        self.progressBar.setValue(25)
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setOrientation(Qt.Orientation.Horizontal)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setTextDirection(QProgressBar.Direction.TopToBottom)
+
+        self.gridLayout_2.addWidget(self.progressBar, 1, 0, 1, 1)
+
+        self.label = QLabel(ProgressDialog)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
+
+        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.pushButtonCancel = QPushButton(ProgressDialog)
+        self.pushButtonCancel.setObjectName(u"pushButtonCancel")
+        self.pushButtonCancel.setStyleSheet(u"QPushButton {\n"
 "    background-color: #0078d7;\n"
 "    color: #ffffff;\n"
 "    padding: 6px;\n"
@@ -59,70 +106,19 @@ class Ui_CalcDialog(object):
 "    padding-bottom: 5px;\n"
 "}")
 
-        self.gridLayout_2.addWidget(self.pushButton, 3, 1, 1, 1)
-
-        self.progressBar = QProgressBar(CalcDialog)
-        self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setStyleSheet(u"QProgressBar {\n"
-"    border: 1px solid #d3d3d3;\n"
-"    border-radius: 4px;\n"
-"    background-color: #ffffff;\n"
-"    text-align: center;\n"
-"    font-family: Arial;\n"
-"    font-size: 12px;\n"
-"    color: #000000;\n"
-"    padding: 2px;\n"
-"}\n"
-"\n"
-"QProgressBar::chunk {\n"
-"    background-color: #0078d7;\n"
-"    border-radius: 2px;\n"
-"    margin: 1px; /* \u041e\u0442\u0441\u0442\u0443\u043f \u0434\u043b\u044f \u0433\u0430\u0440\u043c\u043e\u043d\u0438\u0447\u043d\u043e\u0433\u043e \u0432\u0438\u0434\u0430 \u0432\u043d\u0443\u0442\u0440\u0438 \u0433\u0440\u0430\u043d\u0438\u0446\u044b */\n"
-"}\n"
-"\n"
-"QProgressBar:hover {\n"
-"    background-color: #e0e0e0;\n"
-"}\n"
-"\n"
-"QProgressBar:disabled {\n"
-"    border: 1px solid #d3d3d3;\n"
-"    background-color: #f0f0f0;\n"
-"    color: #808080;\n"
-"}\n"
-"\n"
-"QProgressBar::chunk:disabled {\n"
-"    background-color: #a0a0a0;\n"
-"}")
-        self.progressBar.setValue(24)
-
-        self.gridLayout_2.addWidget(self.progressBar, 2, 0, 1, 2)
-
-        self.lblCalcStatus = QLabel(CalcDialog)
-        self.lblCalcStatus.setObjectName(u"lblCalcStatus")
-        self.lblCalcStatus.setFrameShape(QFrame.Shape.Box)
-        self.lblCalcStatus.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.gridLayout_2.addWidget(self.lblCalcStatus, 0, 0, 1, 2)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout_2.addItem(self.horizontalSpacer, 3, 0, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout_2.addItem(self.verticalSpacer, 1, 0, 1, 2)
+        self.horizontalLayout.addWidget(self.pushButtonCancel)
 
 
-        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
 
 
-        self.retranslateUi(CalcDialog)
+        self.retranslateUi(ProgressDialog)
 
-        QMetaObject.connectSlotsByName(CalcDialog)
+        QMetaObject.connectSlotsByName(ProgressDialog)
     # setupUi
 
-    def retranslateUi(self, CalcDialog):
-        CalcDialog.setWindowTitle(QCoreApplication.translate("CalcDialog", u"Dialog", None))
-        self.pushButton.setText(QCoreApplication.translate("CalcDialog", u"Cancel", None))
-        self.lblCalcStatus.setText(QCoreApplication.translate("CalcDialog", u"Calculation information goes here.", None))
+    def retranslateUi(self, ProgressDialog):
+        ProgressDialog.setWindowTitle(QCoreApplication.translate("ProgressDialog", u"Dialog", None))
+        self.label.setText(QCoreApplication.translate("ProgressDialog", u"lblProgress", None))
+        self.pushButtonCancel.setText(QCoreApplication.translate("ProgressDialog", u"Cancel", None))
     # retranslateUi
