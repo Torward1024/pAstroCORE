4aa3fd9bf8ad57335d77389f550ea9c0004eae4e
Torward1024 <almax1024@gmail.com>
2025-09-23 14:16:56 +0300

    23.09.2025

--- pastrocore/gui/ui_dialog_generate_observations.py
+++ pastrocore/gui/ui_dialog_generate_observations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dialog_generate_observationscUkcsG.ui'
+## Form generated from reading UI file 'dialog_generate_observationsHxWzNS.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.2
 ##
